729900518119f146332e46586cd4bb07fa3aa610
juwon <juwon8891@gmail.com>
2022-06-11 18:27:40 +0900

    1st study

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CompletionException;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
+        int array[] = {1, 2, 3, 4, 5};
+        List<String> list = Arrays.asList("A","B","C");
+        for (String i: list){
+            System.out.println(i);
+        }
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 }
